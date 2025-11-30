8b65d898e9effde4d4da0bc4b6360fef28c615a6
Princess-Dianne <pampola.princessdianne@marsu.edu.ph>
2025-11-30 12:41:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -42,5 +42,7 @@ def predict():
     return jsonify({"prediction": final_pred})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
    
+
